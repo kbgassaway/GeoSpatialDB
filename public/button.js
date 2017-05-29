184d77d185e3e9ca0269c73c559bd5d7c6a65b10
kbgassaway <kbgassaway@gmail.com>
2017-05-29 11:57:29 -0400

    Update button.js for employee job filter
    Creates the filter by employees job title and requests the employees table data for the selected job title.

--- public/button.js
+++ public/button.js
@@ -69,36 +69,38 @@ function createTable(data){
     tBody.setAttribute("id","tableBody");
     console.log(data);
     for(var prop in data){
-        //ID
+        //first name
         console.log(data[prop].first_name);
         row = document.createElement("tr");
         cell = document.createElement("td");
         cell.textContent = data[prop].first_name;
         row.appendChild(cell);
 
-        //first name
+        //last name
         console.log(data[prop].last_name);
         cell = document.createElement("td");
         cell.textContent = data[prop].last_name;
         row.appendChild(cell);
 
-        //last name
+        //job title
         console.log(data[prop].job_title);
         cell = document.createElement("td");
         cell.textContent = data[prop].job_title;
         row.appendChild(cell);
 
-        //last updated
+        //department
         console.log(data[prop].department);
         cell = document.createElement("td");
         cell.textContent = data[prop].department;
         row.appendChild(cell);
-
+        
+        //salary
         console.log(data[prop].salary);
         cell = document.createElement("td");
         cell.textContent = data[prop].salary;
         row.appendChild(cell);
 
+        //office
         console.log(data[prop].office);
         cell = document.createElement("td");
         cell.textContent = data[prop].city;
@@ -114,4 +116,60 @@ function createTable(data){
     tHead.style.backgroundColor = "lightGrey";
     wTable.setAttribute("border", "1pix");
 
+}
+
+
+document.addEventListener('DOMContentLoaded', loadJobTitlesFilter);
+
+//get unique job titles for the filter
+function loadJobTitlesFilter(){
+    var req = new XMLHttpRequest();
+    req.open('GET', '/loadJobTitles', true);
+    req.addEventListener("load", function(){
+        if(req.status >=200 && req.status < 400){
+            var response = JSON.parse(req.responseText);
+            if(response.length)
+                jobTitleOptions(response);
+        } else{
+            console.log("Error in network request: " + req.statusText);
+        }
+    });
+    req.send(null);
+    event.preventDefault();
+}
+
+//creates a dynamic list to filter employees by job title
+function jobTitleOptions(data){
+    var jobTitleFilter = document.getElementById("fltrByJobTitle");
+    for(var prop in data){
+        console.log(data[prop].job_title);
+        var option = document.createElement("option");
+        option.value = data[prop].job_title;
+        option.innerHTML = data[prop].job_title;
+        jobTitleFilter.appendChild(option);
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', filterEmployees);
+
+//filter the table by job title 
+function filterEmployees(){
+    document.getElementById('fltrByJobTitle').addEventListener('change', function(event){
+        var title = document.getElementById('fltrByJobTitle').value;
+	console.log(title);
+        var req = new XMLHttpRequest();
+        req.open('GET', '/fltrByJT?job_title='+ title, true);
+        req.addEventListener("load", function(){
+            if(req.status >=200 && req.status < 400){
+                var response = JSON.parse(req.responseText);
+                if(response.length)
+                    createTable(response);
+            } else{
+                console.log("Error in network request: " + req.statusText);
+            }
+        });
+        req.send(null);
+        event.preventDefault();
+    });
 }
