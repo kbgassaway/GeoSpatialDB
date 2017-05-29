c4be474e8d9d5745565639072f7bc5a0271a5b24
David White <whited6@oregonstate.edu>
2017-05-28 20:28:14 -0400

    added employee query
    employee query added to sql file

--- queries.sql
+++ queries.sql
@@ -2,3 +2,10 @@ SELECT first_name, last_name, job_title, city, cert_name FROM employees e
 LEFT JOIN offices o ON o.id = e.office
 LEFT JOIN employee_cert ec ON ec.eid=e.id
 LEFT JOIN certifications c ON c.id=ec.cid;
+
+
+
+
+--Select the employee data and the city the employee is associated with
+SELECT first_name, last_name, job_title, department, salary, city FROM employees e
+LEFT JOIN offices o ON e.office= o.id; 
